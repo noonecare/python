c890541513d0f0b41f7f27ba4b8568bb4d8bde64
王蒙 <wangmeng@gridsum.com>
2017-04-03 10:12:37 +0800

    patch 也可以用作 decorator， 用做 decorator 和 context manager 是最常见的用法

--- practice/python-test-example/python_test_demo/test_use_mock.py
+++ practice/python-test-example/python_test_demo/test_use_mock.py
@@ -14,3 +14,19 @@ def test_f():
     with patch(target="sys.stdout", new=StringIO()) as fake_out:
         f()
         assert fake_out.getvalue() == 'success\n'
+
+
+def test_f_use_decorator():
+    # patch 也常常写成 decorator, patch decoder 会把 mock 出来的对象(属于 Mock 类， 这一点和 上面用 context manager 方式调
+    # 用 patch 不同， 用 context manager 的方式调用 patch, patch 出来的对象时 new 指定的类型)赋予 所修饰的函数的最后一个参数
+    # patch 的第一个参数应当是 package.module.classname
+    @patch("sys.stdout", new_callable=StringIO)
+    def f_use_decorator(fake_out):
+        f()
+        assert fake_out.getvalue() == "success\n"
+
+    f_use_decorator()
+
+
+if __name__ == '__main__':
+    test_f_use_decorator()
