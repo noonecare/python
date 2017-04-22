306612dcf8541905dfb30cd0b4580aed5295e960
王蒙 <wangmeng@gridsum.com>
2017-04-22 13:33:24 +0800

    解释 tornado.gen.Task 和 add_timeout

--- learn_tornado/add-time-event-to-ioloop.py
+++ learn_tornado/add-time-event-to-ioloop.py
@@ -19,6 +19,11 @@ class MainHandler(tornado.web.RequestHandler):
 
     @tornado.gen.coroutine
     def time_cost_operate(self):
+        """
+        tornado.gen.Task 会在当前的Ioloop iteration 中执行 tornado.gen.Task 参数中的函数，yield tornado.gen.Task 后面的语
+        句和 tornado.gen.Task 参数中函数，是异步执行的
+        add_timeout 会立即执行 epoll 操作
+        """
         yield tornado.gen.Task(tornado.ioloop.IOLoop.instance().add_timeout, time.time() + 100)
         sleep(10)
         print("time cost operate")
