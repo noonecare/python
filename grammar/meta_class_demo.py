11904b169056f7d5bb7dccda0913385d7425c188
王蒙 <wangmeng@gridsum.com>
2017-04-23 14:47:48 +0800

    metaclass example

---
+++ grammar/meta_class_demo.py
@@ -0,0 +1,15 @@
+
+def authenticate(future_class_name, future_class_parents, future_class_attr):
+    future_class_attr.update({'author': 'wangmeng'})
+    return type(future_class_name, future_class_parents, future_class_attr)
+
+
+# 创建类时，默认的 metaclass 是 type, 传给type 的三个参数是 future_class_name, future_class_parents, future_class_attr，
+# type 返回的是个 type 类型的 instance(其实就是 class)
+class Demo(metaclass=authenticate):
+    def greet(self):
+        print("Hello")
+
+
+Demo().greet()
+print(Demo.author)
