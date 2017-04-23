282b916ae20e916d0805437eca62d85c8923040d
王蒙 <wangmeng@gridsum.com>
2017-04-23 14:55:27 +0800

    metaclass 不一定要写成类，但是最常见的是写成类，这里提供一个类做 metaclass

--- grammar/meta_class_demo.py
+++ grammar/meta_class_demo.py
@@ -4,9 +4,16 @@ def authenticate(future_class_name, future_class_parents, future_class_attr):
     return type(future_class_name, future_class_parents, future_class_attr)
 
 
+class AuthenticateClass(type):
+
+    def __new__(cls, future_class_name, future_class_parents, future_class_attr):
+        future_class_attr.update({'author': 'wangmeng'})
+        return type(future_class_name, future_class_parents, future_class_attr)
+
+
 # 创建类时，默认的 metaclass 是 type, 传给type 的三个参数是 future_class_name, future_class_parents, future_class_attr，
 # type 返回的是个 type 类型的 instance(其实就是 class)
-class Demo(metaclass=authenticate):
+class Demo(metaclass=AuthenticateClass):
     def greet(self):
         print("Hello")
 
